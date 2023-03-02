e22676a626768201e0b6450e09d4697b57814955
1amSumit <sj779619@gmail.com>
2023-03-02 21:48:36 +0530

    login

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const userRoutes = require("./routes/userRoutes");
 const app = express();
 
 app.use(express.json());
-
+console.log("hu hai");
 app.use("/api/user", userRoutes);
 
 module.exports = app;
